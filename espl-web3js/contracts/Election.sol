4a29abe8d7b9fdc91364bd08d54f29ebf6868acf
martinobettucci <martino.bettucci@gmail.com>
2023-11-07 10:20:47 +0100

    sources pour le cours

---
+++ espl-web3js/contracts/Election.sol
@@ -0,0 +1,55 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.17;
+
+contract Election {
+
+    // Read/write candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    address owner;
+
+    mapping(address => bool) public voters;
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
+
+    // Constructor
+    constructor () {
+        owner = msg.sender;
+    }
+
+    modifier only_unvoted() {
+        require(!voters[msg.sender]);
+        _;
+    }
+
+    modifier only_candidates(uint _candidateId) {
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        _;
+    }
+
+    function vote (uint _candidateId) external only_unvoted() only_candidates(_candidateId) {
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+    }
+
+    modifier only_owners() {
+        require(owner == msg.sender);
+        _;
+    }
+
+    function addCandidate (string calldata _name) public only_owners() {
+        candidatesCount++;
+        Candidate storage c = candidates[candidatesCount];
+        c.id = candidatesCount;
+        c.name = _name;
+        c.voteCount = 0;
+    }
+}
